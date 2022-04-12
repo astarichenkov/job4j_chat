4f1065b40a50444ac45cc2965f7a08d2f92af4cd
Anton.Starichenkov <anton.starichenkov@gmail.com>
2022-04-12 09:17:31 +0300

    6. ResponseEntity [#504799]

--- src/main/java/ru/job4j/chat/controller/RoomController.java
+++ src/main/java/ru/job4j/chat/controller/RoomController.java
@@ -1,6 +1,7 @@
 package ru.job4j.chat.controller;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,8 +23,10 @@ public class RoomController {
     }
 
     @GetMapping("/")
-    public List<Room> findAll() {
-        return rooms.findAll();
+    public ResponseEntity<List<Room>> findAll() {
+        List<Room> list = rooms.findAll();
+        return list.isEmpty() ? ResponseEntity.status(HttpStatus.NOT_FOUND).build()
+                : new ResponseEntity<>(list, HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
